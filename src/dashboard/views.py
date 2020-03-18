df53e29e0f4fa380702cdcf487139a1df96b0e41
Sarang Chougs <sarangchouguley284@gmail.com>
2020-03-18 22:38:59 +0530

    Added energystatus and select site template

--- src/dashboard/views.py
+++ src/dashboard/views.py
@@ -5,3 +5,6 @@ from django.shortcuts import render
 #view for dashboard.html
 def dashboard_home(request):
     return(render(request,'dashboard.html'))
+
+def dashboard_energy_status(request):
+    return(render(request,'energystatus.html'))
--- src/ems/urls.py
+++ src/ems/urls.py
@@ -15,9 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from dashboard.views import dashboard_home
+from dashboard.views import dashboard_home,dashboard_energy_status
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('dashboard', dashboard_home)
+    path('dashboard', dashboard_home),
+    path('energystatus', dashboard_energy_status),
 ]
